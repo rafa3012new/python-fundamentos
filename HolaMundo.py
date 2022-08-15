fa422a0ca7e06e5e5fbd861173e7eae14fcb9670
rafa3012new <92983803+rafa3012new@users.noreply.github.com>
2022-08-15 18:41:40 -0400

    Add files via upload

---
+++ HolaMundo.py
@@ -0,0 +1,21 @@
+#1
+print("Hola Mundo")
+minombre = "Rafael"
+#2a
+print(f"Hola Mundo, Mi nombre es, {minombre}")
+#2b
+miapellido = "Fernandez"
+print(f"Hola Mundo, Mi nombre es {minombre} " + f"{miapellido}")
+#3a
+numero = 7
+print(f"Hola numero, {numero}")
+#3b
+numero = 7
+print(f"Hola numero," + f"{numero}")
+comida1 = "pizza"
+comida2 = "ceviche"
+#4a
+txt = "amo comer {} y {}"
+print(txt.format(comida1, comida2))
+#4b
+print(f"amo comer {comida1}" + " y " + f"{comida2}")
