60698c412fd78a03f3dd031a4b19ee53869ec0b3
rafa3012new <92983803+rafa3012new@users.noreply.github.com>
2022-08-16 06:46:08 -0400

    Add files via upload

---
+++ funcionesbasicasI.py
@@ -0,0 +1,132 @@
+#1 DEVOLVERA 5 Y SE IMPRIMIRA 5
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+
+#2 DARA ERROR
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+
+#3 DEVOLVERA 5 Y SE IMPRIMIRA 5
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+
+
+# #4 DEVOLVERA 5 Y SE IMPRIMIRA 5
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+
+
+#5 IMPRIMIRA 5, IMPRIMIRA NONE
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+
+
+#6 IMPRIMIRA 3 IMPRIMIRA 5 IMPRIMIRA 8
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+
+#7 DEVOLVERA LA CONCATENACION '2' + '5' E IMPRIMIRA 25
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+
+
+#8 IMPRIMIRA 100 DEVOLVERA 10 E IMPRIMIRA 10
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+
+
+#9 DEVOLVERA E IMPRIMIRA 7, DEVOLVERA E IMPRIMIRA 14, DEVOLVERA 7 Y 14 E IMPRIMIERA 21
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+
+
+#10 DEVOLVERA E IMPRIMIRA 8 
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+
+
+#11 IMPRIMIRA 500, IMPRIMIRA 500, IMPRIMIRA 300, IMPRIMIRA 500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+
+
+#12 IMPRIMIRA 500, IMPRIMIRA 500, IMPRIMIRA 300, IMPRIMIRA 500
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+
+
+#13 IMPRIMIRA 500, IMPRIMIRA 500, IMPRIMIRA 300, IMPRIMIRA 300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+
+
+#14 IMPRIMIRA 1, IMPRIMIRA 3, IMPRIMIRA 2
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+
+
+#15 IMPIRMIRA 1, IMPRIMIRA 3, DEVOLVERA E IMPRIMIRA 5, DEVOLVERA E IMPIRMIERA 10
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
